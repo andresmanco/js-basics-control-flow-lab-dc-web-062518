a04209b8233925918ad5ffd6fdbeccdf7c3a758a
github username <github email address>
2018-08-04 13:49:27 -0400

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,25 @@ function scuberGreetingForFeet(number)
   {
     return 'I will gladly take your thirty bucks.';
   }
+}
 
+function ternaryCheckCity(city)
+{
+  if (city === 'NYC') {
+      return 'Ok, sounds good.';
+  }else {
+    return 'No go.';
+  }
+}
+
+function switchOnCharmFromTip(tip)
+{
+  if (tip === 'generous')
+  {
+    return 'Thank you so much.';
+  }else if (tip === 'not as generous') {
+    return 'Thank you.';
+  }else {
+    return 'Bye.';
+  }
 }
